e2623d3214da77819f6bbca833d8c120939f18c8
Tamanit <78948032+Tamanit@users.noreply.github.com>
2023-12-01 15:43:06 +0500

    контроллер

--- server/PIS-Vaccination-PI-21-03/Source/Controller/LogReaderController.cs
+++ server/PIS-Vaccination-PI-21-03/Source/Controller/LogReaderController.cs
@@ -1,6 +1,36 @@
-﻿namespace PIS_Vaccination_PI_21_03.Source.Controller;
-//TODO: читающий логгер
-public class LogReaderController
+﻿using Microsoft.AspNetCore.Mvc;
+using PIS_Vaccination_PI_21_03.Source.Repository;
+using PIS_Vaccination_PI_21_03.Source.Models.ViewModels;
+using PIS_Vaccination_PI_21_03.Source.Services.Permission;
+
+namespace PIS_Vaccination_PI_21_03.Source.Controller;
+
+[ApiController]
+[Route("/api/v1/[controller]/[action]")]
+public class LogReaderController : ControllerBase
 {
+    [HttpGet]
+    [ActionName("read")]
+    public async Task<IActionResult> Read(int id) =>
+        Ok((LogViewModel)LogRepository.Read(id));
+
+    [HttpPost]
+    [ActionName("create")]
+    public async Task<IActionResult> Create([FromBody] LogViewModel newLog) =>
+        Ok(LogRepository.Create(newLog));
     
+    [HttpPut] 
+    [ActionName("update")]
+    public async Task<IActionResult> Update([FromBody] LogViewModel logModel) => 
+        Ok((LogViewModel)LogRepository.Update(logModel));
+
+    [HttpDelete]
+    [ActionName("delete")]
+    public async Task<IActionResult> Delete(int id) =>
+        Ok(LogRepository.Delete(id));
+    
+    [HttpGet]
+    [ActionName("list")]
+    public async Task<IActionResult> List() =>
+        Ok(LogRepository.List().Select(x => (LogViewModel)x).ToList());
 }
--- server/PIS-Vaccination-PI-21-03/Source/Middleware/LogWriterMiddleware.cs
+++ server/PIS-Vaccination-PI-21-03/Source/Middleware/LogWriterMiddleware.cs
@@ -3,5 +3,19 @@
 //TODO: пишущий логер
 public class LogWriterMiddleware
 {
-    
+    private readonly RequestDelegate _next;  
+    private readonly ILogger<LogWriterMiddleware> _logger;  
+    public LogWriterMiddleware(RequestDelegate next, ILoggerFactory loggerFactory)  
+    {  
+        _next = next;  
+        _logger = loggerFactory?.CreateLogger<LogWriterMiddleware>() ??  
+                  throw new ArgumentNullException(nameof(loggerFactory));  
+    }  
+    public async Task InvokeAsync(HttpContext context)  
+    {  
+        string controllerName = context.Request.RouteValues["controller"].ToString();
+        string actionName = context.Request.RouteValues["action"].ToString();
+        
+        
+    }
 }
