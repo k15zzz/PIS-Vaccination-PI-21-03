5b28ee02383ec795cd0b36c85fccaac48f2fea01
literrarian <razdolbaika1@gmail.com>
2023-12-01 14:18:03 +0500

    log entity model

--- server/PIS-Vaccination-PI-21-03/Source/Models/EntitiesModels/LogEntitiesModel.cs
+++ server/PIS-Vaccination-PI-21-03/Source/Models/EntitiesModels/LogEntitiesModel.cs
@@ -1,9 +1,22 @@
 ﻿using System.ComponentModel.DataAnnotations.Schema;
 
 namespace PIS_Vaccination_PI_21_03.Source.Models;
-//TODO: написать модельку
-//[Table("log")]
-//public class LogEntitiesModel
-//{
-    
-//}
+
+[Table("logging")]
+public class LogEntitiesModel
+{
+    [Column("id")] public int Id { get; set; }
+    [Column("date_time")] public DateTime LogDate { get; set; }
+    [Column("object_instance_id")] public string ObjId { get; set; }
+    [Column("object_description_after_action")] public string ObjDescr { get; set; }
+    [ForeignKey("fk_user")] public OrganizationEntitiesModel UserId { get; set; }
+    public static implicit operator LogEntitiesModel(LogViewModel viewModel)
+    {
+        LogEntitiesModel _entetyModel = new LogEntitiesModel();
+        _entetyModel.Id = viewModel.Id;
+        _entetyModel.LogDate = DateTime.SpecifyKind(viewModel.LogDate, DateTimeKind.Utc);
+        _entetyModel.ObjId = viewModel.ObjId;
+        _entetyModel.ObjDescr = viewModel.ObjDescr;
+        return _entetyModel;
+    }
+}
