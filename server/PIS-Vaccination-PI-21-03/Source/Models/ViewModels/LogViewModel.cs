5a478eb6cd234c1ba759e6bee113ec527918d267
literrarian <razdolbaika1@gmail.com>
2023-12-01 15:41:00 +0500

    small fixes

--- server/PIS-Vaccination-PI-21-03/Source/Models/ViewModels/LogViewModel.cs
+++ server/PIS-Vaccination-PI-21-03/Source/Models/ViewModels/LogViewModel.cs
@@ -1,6 +1,6 @@
 ﻿using PIS_Vaccination_PI_21_03.Source.Repository;
 
-namespace PIS_Vaccination_PI_21_03.Source.Models.ViewModels;
+namespace PIS_Vaccination_PI_21_03.Source.Models;
 
 public class LogViewModel
 {
@@ -8,7 +8,7 @@ public class LogViewModel
     public DateTime LogDate { get; set; }
     public string ObjId { get; set; }
     public string ObjDescr { get; set; }
-    public int FkUserId { get; set; }
+    public int FkUser { get; set; }
     public string name;
     public string surname;
     public string patronymic;
@@ -34,18 +34,18 @@ public class LogViewModel
       //  m.FkUserId = entitiesModel.FkUserId;
         using (var db = new AppDbContext())
         {
-            m.name = db.Users.Find(entitiesModel.FkUserId).Name;
-            m.surname = db.Users.Find(entitiesModel.FkUserId).Surname;
-            m.patronymic = db.Users.Find(entitiesModel.FkUserId).Patronymic;
-            m.phone = db.Users.Find(entitiesModel.FkUserId).Phone;
-            m.email = db.Users.Find(entitiesModel.FkUserId).Email;
-            m.workPhone = db.Users.Find(entitiesModel.FkUserId).WorkPhone;
-            m.workEmail = db.Users.Find(entitiesModel.FkUserId).workEmail;
-            m.login = db.Users.Find(entitiesModel.FkUserId).Login;
+            m.name = db.Users.Find(entitiesModel.FkUser).Name;
+            m.surname = db.Users.Find(entitiesModel.FkUser).Surname;
+            m.patronymic = db.Users.Find(entitiesModel.FkUser).Patronymic;
+            m.phone = db.Users.Find(entitiesModel.FkUser).Phone;
+            m.email = db.Users.Find(entitiesModel.FkUser).Email;
+            m.workPhone = db.Users.Find(entitiesModel.FkUser).WorkPhone;
+            m.workEmail = db.Users.Find(entitiesModel.FkUser).WorkEmail;
+            m.login = db.Users.Find(entitiesModel.FkUser).Login;
          //   userOrg = db.Users.Find(entitiesModel.FkUserId).FkOrg;
             m.FkOrg = db.Users.Find(entitiesModel.FkUserId).FullName;
             
         }
-        return _entetyModel;
+        return m;
     }
 }
