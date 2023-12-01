4cf1b018f685f65fc3473392780fb286f42514c0
literrarian <razdolbaika1@gmail.com>
2023-12-01 16:39:00 +0500

    vue log repo+model
    idk how to do controller

--- client/src/controller/LogReaderController.js
+++ client/src/controller/LogReaderController.js
@@ -1,1 +1,89 @@
-//TODO: сделать эту хуетуз
+import {reactive} from 'vue';
+import {LogRepository} from "../repositorys/LogRepository.js";
+import {HelperController} from "./HelperController.js";
+import {ContractRepository} from "../repositorys/ContractRepository.js";
+
+export class AnimalController extends HelperController {
+    backLink = '/animal-registry'
+    value = reactive({
+        id: null,
+        regNum: "",
+        sex: false,
+        yearBirth: null,
+        electronicChipNumber: "",
+        name: "",
+        specialMarks: null,
+        category: null,
+        town: null
+    });
+    townList =  reactive([]);
+    categoryList =  reactive([]);
+
+    constructor(type, id = 0) {
+        super(type, id);
+    }
+
+    async read(id) {
+        const model = await AnimalRepository.get(id);
+        this.value.id = model.id;
+        this.value.regNum = model.regNum;
+        this.value.sex = model.sex;
+        this.value.yearBirth = model.yearBirth;
+        this.value.electronicChipNumber = model.electronicChipNumber;
+        this.value.name = model.name;
+        this.value.specialMarks = model.specialMarks;
+        this.value.town = {
+            id: model.fkTown,
+            name: model.town,
+        };
+        this.value.category = {
+            id: model.fkAnimalCategory,
+            name: model.animalCategory,
+        };
+    }
+
+    async load() {
+        this.townList.push(...await AnimalRepository.townList());
+        this.categoryList.push(...await AnimalRepository.categoryList());
+    }
+
+    async delete(id) {
+        let resp = await AnimalRepository.delete(id);
+        if (resp) {
+            this.router.push(this.backLink);
+        }
+    }
+
+    async update() {
+        let resp =  await AnimalRepository.update(
+            this.value.id,
+            this.value.regNum,
+            this.value.sex,
+            this.value.yearBirth,
+            this.value.electronicChipNumber,
+            this.value.name,
+            this.value.specialMarks,
+            this.value.category.id,
+            this.value.town.id,
+        );
+        if (resp) {
+            this.router.push(this.backLink);
+        }
+    }
+
+    async create() {
+        let resp = await AnimalRepository.create(
+            this.value.regNum,
+            this.value.sex,
+            this.value.yearBirth,
+            this.value.electronicChipNumber,
+            this.value.name,
+            this.value.specialMarks,
+            this.value.category.id,
+            this.value.town.id,
+        );
+        if (resp) {
+            this.router.push(this.backLink);
+        }
+    }
+}
---
+++ client/src/repositorys/LogRepository.js
@@ -0,0 +1,30 @@
+﻿import {SerializeService} from "../services/SerializeService.js";
+import {LogModel} from "../models/LogModel.js";
+import {RequestService} from "../services/RequestService.js";
+
+export class LogRepository {
+    static async list() {
+        const response = await fetch("/api/v1/logreader/list");
+
+        let list = [];
+
+        let rawList = await response.json()
+
+        rawList.forEach((row) => {
+            let model = SerializeService.serialize(row, new LogModel());
+            list.push(model)
+        })
+
+        return list;
+    }
+
+    static async delete(id) {
+        return await RequestService.Delete('/api/v1/logreader/delete', id);
+    }
+    
+    static async get(id) {
+        const row = await RequestService.Get('/api/v1/logreader/read?id='+id);
+        return SerializeService.serialize(row, new LogModel());
+    }
+    //todo: экспорт
+}
