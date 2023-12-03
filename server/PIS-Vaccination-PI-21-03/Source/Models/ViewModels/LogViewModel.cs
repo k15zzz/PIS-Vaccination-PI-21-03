a3e9e6d6315a585f9586d3f242c14a01db796a45
literrarian <razdolbaika1@gmail.com>
2023-12-03 18:50:40 +0500

    changes in log model.cs

--- server/PIS-Vaccination-PI-21-03/Source/Models/ViewModels/LogViewModel.cs
+++ server/PIS-Vaccination-PI-21-03/Source/Models/ViewModels/LogViewModel.cs
@@ -6,44 +6,40 @@ public class LogViewModel
 {
     public int Id { get; set; }
     public DateTime LogDate { get; set; }
-    public string ObjId { get; set; }
+    public int ObjId { get; set; }
     public string ObjDescr { get; set; }
-    public string name { get; set; }
-    public string surname { get; set; }
-    public string patronymic { get; set; }
-    public string phone { get; set; }
-    public string email { get; set; }
-    public string workPhone { get; set; }
-    public string workEmail { get; set; }
-    public string login { get; set; }
-    public string OrgName { get; set; }
-    public string RoleName { get; set; }
-    public int FkUser { get; set; }
+    public string Name { get; set; }
+    public string Surname { get; set; }
+    public string Patronymic { get; set; }
+    public string Phone { get; set; }
+    public string Email { get; set; }
+    public string WorkPhone { get; set; }
+    public string WorkEmail { get; set; }
+    public string Login { get; set; }
+    public string Organization { get; set; }
+    public string Position { get; set; }
+    
 
-    public static implicit operator LogViewModel(EntitiesModels.LogEntitiesModel entitiesModel)
+    public static implicit operator LogViewModel(EntitiesModels.LogEntitiesModel entetyModel)
     {
         var m = new LogViewModel
         {
-            Id = entitiesModel.Id,
-            LogDate = entitiesModel.LogDate,
-            ObjId = entitiesModel.ObjId,
-            ObjDescr = entitiesModel.ObjDescr
+            Id = entetyModel.Id,
+            Surname = entetyModel.Surname,
+            Name = entetyModel.Name,
+            Patronymic = entetyModel.Patronymic,
+            Phone = entetyModel.Phone,
+            Email = entetyModel.Email,
+            Organization = entetyModel.Organization,
+            Position = entetyModel.Position,
+            WorkPhone = entetyModel.WorkPhone,
+            WorkEmail = entetyModel.WorkEmail,
+            Login = entetyModel.Login,
+            LogDate = DateTime.SpecifyKind(entetyModel.LogDate, DateTimeKind.Utc),
+            ObjId = entetyModel.ObjId,
+            ObjDescr = entetyModel.ObjDescr
         };
-
-        using (var db = new AppDbContext())
-        {
-            var _user = db.Users.Find(entitiesModel.FkUser);
-            m.name = _user.Name;
-            m.surname = _user.Surname;
-            m.patronymic = _user.Patronymic;
-            m.phone = _user.Phone;
-            m.email = _user.Email;
-            m.workEmail = _user.WorkEmail;
-            m.workPhone = _user.WorkEmail;
-            m.login = _user.Login;
-            m.RoleName = db.Roles.Find(_user.FkRole).Name;
-            m.OrgName = db.Organizations.Find(_user.FkOrg).FullName;
-        }
+        
         return m;
     }
 }
