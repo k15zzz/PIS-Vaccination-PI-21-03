5b94ad9a0ff1bbb28ac9bf8eb7a84750031dc7fe
Tamanit <78948032+Tamanit@users.noreply.github.com>
2023-12-07 22:35:17 +0500

    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

--- server/PIS-Vaccination-PI-21-03/Source/Middleware/LogWriterMiddleware.cs
+++ server/PIS-Vaccination-PI-21-03/Source/Middleware/LogWriterMiddleware.cs
@@ -5,16 +5,9 @@ namespace PIS_Vaccination_PI_21_03.Source.Middleware;
 //TODO: пишущий логер
 public class LogWriterMiddleware : IMiddleware
 {
-
-    // nixua ne rabotaet
-    //TODO: sdelat chtob rabotalo
-    public async Task InvokeAsync(HttpContext context, RequestDelegate next)  
-    {  
-        using (StreamWriter writer = new StreamWriter("/Users/tamanit/Desktop/test.txt"))
-        {
-            writer.WriteLineAsync("I work!");
-        }
-
+    public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+    {
+        Console.WriteLine("Middleware worked!!!");
         await next(context);
     }
 }
